092846163e84a1e9a54363e3324a0aebb991a287
Isaac Dowell <izak.dowell@gmail.com>
2025-04-30 21:39:59 -0500

    Update the python GUI: deleted gui_controller.py, attempting to set the current gui to a more OOP approach for the next push.

--- main/database/connection.py
+++ main/database/connection.py
@@ -1,3 +1,19 @@
 """
 SCRIPT FOR CONNECTING TO THE DATABASE
 """
+
+import mysql.connector
+
+# Tries to connect to the DB
+# Success: Print a message saying the connection was successful to the terminal
+# Fail: Print error message to the terminal
+try:
+    connection = mysql.connector.connect(
+        host="localhost",
+        user = "root",
+        password = "password",
+        database = "cfg")
+
+    print('Connected to MySQL Database')
+except mysql.connector.Error as error:
+    print('Failed to connect to MySQL database: {}'.format(error))
--- main/driver/main.py
+++ main/driver/main.py
@@ -1,3 +1,8 @@
 """
 APPLICATION DRIVER
 """
+
+# Global set def: application state
+# Possible app states => IDLE, MANAGE, REPORT, CONNECTING
+ALLOWED_APP_STATES = {'REPORT', 'MANAGE', 'IDLE', 'CONNECTING'}
+CURRENT_STATE = 'IDLE'
